d969b48549413785657a17e6fa898c5e3d2d6a07
NKDuy <kn145660@gmail.com>
2022-11-02 10:55:57 +0700

    first commit

---
+++ src/components/index.ts
@@ -0,0 +1,1 @@
+export * from "./rindo-generated/index";
---
+++ src/components/rindo-generated/index.ts
@@ -0,0 +1,30 @@
+/* 
+
+auto-generated react proxies will replace this file 
+if you use "../your-react-library-name/src/components/rindo-generated/index.ts" for your proxies target:
+
+*/
+
+/* SAMPLE: 
+import { reactOutputTarget as react } from '@rindo/react-output-target'
+
+export const config: Config = {
+  outputTargets: [
+    react({
+      componentCorePackage: 'your-rindo-library-name',
+      proxiesFile: '../your-react-library-name/src/components/rindo-generated/index.ts',
+      includeDefineCustomElements: true,
+    });
+    {
+      type: 'dist',
+      esmLoaderPath: '../loader',
+    },
+    {
+      type: 'dist-custom-elements',
+      dir: "./dist/custom-elements"
+    },
+  ],
+}
+*/
+
+export const object = {};
